81c2bf012a5346d856918914b2a7f634c91dad0d
Martastain <martas@imm.cz>
2015-09-25 17:26:26 +0200

    Added vendor update script

---
+++ vendor.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+VENDORDIR=$BASEDIR"/vendor"
+VENDORLST=$BASEDIR"/vendor.lst"
+
+if [ ! -f $VENDORLST ]; then
+    echo "Vendor modules list (vendor.lst) not found"
+    exit 1
+fi
+
+if [ ! -d $VENDORDIR ]; then
+    mkdir $VENDORDIR
+fi
+
+
+while read repo; do
+
+    RNAME=`basename $repo`
+    DNAME=$VENDORDIR"/"$RNAME
+
+    if [ -d $DNAME ] && [ -d $DNAME"/.git" ]; then
+        echo ""
+        echo "Updating module $RNAME"
+        cd $DNAME
+        git pull
+    elif [ ! -d $DNAME ]; then
+        echo ""
+        echo "Downloading module $RNAME"
+        cd $VENDORDIR
+        git clone $repo
+    fi
+
+done < $VENDORLST
+
