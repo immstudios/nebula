7f734c70a4107aac9d8760027158e8d80fecf6d5
Martastain <martas@imm.cz>
2015-10-09 12:24:58 +0200

    Added fix_permissions.sh

---
+++ fix_permissions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+chmod 755 mam 
+chmod 644 mam/*.py
+
+chmod 755 nebula.py
+chmod 644 toolset.py
+chmod 755 run_service.py
+
+chmod 755 vendor/
+chmod 755 vendor.sh
+chmod 644 vendor.lst
+
+chmod 644 .gitignore
+chmod 644 LICENSE
+chmod 644 README.md
