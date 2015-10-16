ada582692c4a73195d1dceb2a889295bc1ec43d9
Martastain <martas@imm.cz>
2015-10-16 11:44:07 +0200

    Dispatch skeleton

--- nebula.py
+++ nebula.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from toolset import *
+from __future__ import print_function
 
-logging.user = "Dispatch"
-logging.info("Starting nebula")
+import os
+import sys
+
+if sys.version_info[:2] < (3, 0):
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
+#
+# vendor imports
+#
+
+for pname in os.listdir("vendor"):
+    pname = os.path.join("vendor", pname)
+    pname = os.path.abspath(pname)
+    if not pname in sys.path:
+        sys.path.append(pname)  
+
+from nxtools import *
+from mam import *
+
+logging.user = "Nebula"
 
 
+#
+# Nebula dispatch
+#
 
-try:
-    hovno
-except:
-    log_error()
+
+def start_nebula():
+    logging.user = "Dispatch"
+    logging.info("Starting nebula")
+
+#
+# Start dispatch only if this script is executed (not imported)
+#
+
+
+if __name__ == "__main__":
+    start_nebula()
