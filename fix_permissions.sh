1bed485fa853f2b870bf78d5f542e48419bffa40
Martastain <martas@imm.cz>
2015-11-02 14:37:18 +0100

    renamed service starting script

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -4,8 +4,13 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 chmod 755 mam 
 chmod 644 mam/*.py
 
+chmod 755 services
+
+find $BASEDIR/services -type d -exec chmod 775 {} +
+find $BASEDIR/services -type f -exec chmod 664 {} +
+
 chmod 755 nebula.py
-chmod 755 run_service.py
+chmod 755 service.py
 
 chmod 755 vendor/
 chmod 755 vendor.sh
