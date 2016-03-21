1efb51ae364bb7d427f5afc6726a31030f6dc3dc
Martastain <martas@imm.cz>
2016-03-21 15:39:18 +0100

    reading /etc/nebula.json config

--- nx/core/common.py
+++ nx/core/common.py
@@ -46,17 +46,28 @@ class Config(dict):
         self["site_name"] = "Unnamed"
         self["user"] = "Nebula"              # Service identifier. Should be overwritten by service/script.
         self["host"] = socket.gethostname()  # Machine hostname
+
         if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
             local_settings_path = sys.argv[1]
         else:
             local_settings_path = "local_settings.json"
-        try:
-            local_settings = json.loads(open(local_settings_path).read())
-        except:
-            print (local_settings_path)
-            log_traceback(handlers=False)
-            critical_error("Unable to open site_settings file.")
-        self.update(local_settings)
+
+        settings_files = [
+                    "/etc/nebula.json",
+                    local_settings_path
+                ]
+        settings = {}
+        for settings_file in settings_files:
+            if os.path.exists(settings_file):
+                try:
+                    settings.update(json.load(open(settings_file)))
+                    logging.debug("Parsing {}".format(local_settings_path), handlers=False)
+                except:
+                    log_traceback(handlers=False)
+
+        if not settings:
+            critical_error("Unable to open site settings")
+        self.update(settings)
 
 config = Config()
 
