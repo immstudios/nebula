f85b2cf5782ed0b230467bc5cd83f993905bc611
Martastain <martas@imm.cz>
2016-03-27 09:59:25 +0200

    added license

--- nebula.py
+++ nebula.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#    This file is part of Nebula media asset management.
+#
+#    Nebula is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Nebula is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 from __future__ import print_function
 
@@ -59,13 +75,13 @@ if __name__ == "__main__":
 
     agents = []
 
-    for agent in [Admin, StorageMonitor, ServiceMonitor, SystemMonitor]:
+    for Agent in [Admin, StorageMonitor, ServiceMonitor, SystemMonitor]:
         try:
-            agents.append(agent())
+            agents.append(Agent())
         except:
             log_traceback()
             shutdown(agents)
-            critical_error("Unable to start Nebula")
+            critical_error("Unable to start {}".format(Agent.__name__))
 
     while True:
         try:
