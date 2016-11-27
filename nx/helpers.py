84bef3224e23d8c178b0459bea98a78bbd3e4276
Martastain <martas@imm.cz>
2016-11-27 14:01:08 +0100

    removed old get_assets

--- nx/helpers.py
+++ nx/helpers.py
@@ -1,41 +1,6 @@
 from .core import *
-from .db import *
+from .connection import *
 from .objects import *
-
-__all__ = [
-        "get_assets",
-        "get_user",
-        "asset_by_path",
-        "asset_by_full_path",
-    ]
-
-
-def get_assets(fulltext="", **kwargs):
-    db = DB()
-    conds = []
-
-    for key in kwargs:
-        value = kwargs[key]
-        if type(value) == list:
-            operator = "IN"
-            value = "({})".format(", ".join("'{}'".format(value) for value in value))
-        else:
-            operator = "="
-            value = "'{}'".format(value)
-        conds.append("meta->>'{}' {} {}".format(key, operator, value))
-
-    if fulltext:
-        ft = slugify(fulltext, make_set=True)
-        conds.extend(["ft_index LIKE '%{}%'".format(elm) for elm in ft])
-
-    conds = " AND ".join(conds)
-    if conds:
-        conds = "WHERE " + conds
-    q = "SELECT meta FROM assets {}".format(conds)
-    logging.debug("Executing browse query:", q)
-    db.query(q)
-    for meta, in db.fetchall():
-        yield Asset(meta=meta)
 
 
 def get_user(login, password, db=False):
