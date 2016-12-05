63e5820713a20d4817ac2495ddeb1ecddf65a80a
Martastain <martas@imm.cz>
2016-12-05 21:20:46 +0100

    switched to rex

--- nebula.py
+++ nebula.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 import os
 import sys
+import rex
 
 #
 # Env setup
@@ -29,22 +30,9 @@ if sys.version_info[:2] < (3, 0):
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-nebula_root = os.path.abspath(os.getcwd())
-
-#
-# Vendor imports
-#
-
-vendor_dir = os.path.join(nebula_root, "vendor")
-if os.path.exists(vendor_dir):
-    for pname in os.listdir(vendor_dir):
-        pname = os.path.join(vendor_dir, pname)
-        pname = os.path.abspath(pname)
-        if not pname in sys.path:
-            sys.path.insert(0, pname)
-
 from nx import *
-config["nebula_root"] = nebula_root
+
+config["nebula_root"] = os.path.abspath(os.getcwd())
 
 #
 # Start agents only if this script is executed (not imported)
