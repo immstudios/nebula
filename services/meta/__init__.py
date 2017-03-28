c51e0c1879c060e87b9622818d501dba0763d0f4
Martastain <martas@imm.cz>
2017-03-28 08:42:56 +0200

    meta service - first working version

--- services/meta/__init__.py
+++ services/meta/__init__.py
@@ -7,17 +7,13 @@ probes = [
 
 class Service(BaseService):
     def on_init(self):
-        filters = []
-        if filters:
-            self.filters = "AND " + " AND ".join(filters)
-        else:
-            self.filters = ""
+        pass
 
     def on_main(self):
         self.mounted_storages = []
         for id_storage in storages:
-            sp = storages[id_storage].local_path
-            if os.path.exists(sp) and len(os.listdir(sp)) != 0:
+            storage_path = storages[id_storage].local_path
+            if os.path.exists(storage_path) and len(os.listdir(storage_path)) != 0:
                 self.mounted_storages.append(id_storage)
 
         db = DB()
@@ -25,21 +21,16 @@ class Service(BaseService):
         db.query("SELECT id, meta FROM assets WHERE media_type=%s AND status NOT IN (3, 4)", [FILE])
         for id, meta in db.fetchall():
             asset = Asset(meta=meta, db=db)
-            self._proc(asset)
-
-    def _proc(self, asset):
-        fname = asset.file_path
+            self.process(asset)
 
 
-        return
-        #
-        # TODO:
-        #
-
+    def process(self, asset):
+        full_path = asset.file_path
         if asset["id_storage"] not in self.mounted_storages:
+            print "skipping unmounted storage", asset["id_storage"]
             return
 
-        if not os.path.exists(fname):
+        if not os.path.exists(full_path):
             if asset["status"] in [ONLINE, RESET, CREATING]:
                 logging.warning("Turning offline {} (File does not exist)".format(asset))
                 asset["status"] = OFFLINE
@@ -47,10 +38,10 @@ class Service(BaseService):
             return
 
         try:
-            fmtime = int(os.path.getmtime(fname))
-            fsize  = int(os.path.getsize(fname))
+            fmtime = int(os.path.getmtime(full_path))
+            fsize  = int(os.path.getsize(full_path))
         except Exception:
-            log_traceback("Unable to get file attrs {}".format(asset))
+            log_traceback("Unable to get file attributes {}".format(asset))
             return
 
         if fsize == 0:
@@ -62,9 +53,9 @@ class Service(BaseService):
 
         if fmtime != asset["file/mtime"] or asset["status"] == RESET:
             try:
-                f = open(fname,"rb")
+                f = open(full_path, "rb")
             except Exception:
-                logging.debug("{} creation in progress.".format(asset))
+                logging.debug("{} is not accessible (creation in progress?)".format(asset))
                 return
             else:
                 f.seek(0,2)
@@ -86,22 +77,17 @@ class Service(BaseService):
 
                 keys = list(asset.meta.keys())
                 for key in keys:
-                    if meta_types[key]["namespace"] in ("fmt", "qc"):
+                    if meta_types[key]["ns"] in ("f", "q"):
                         del (asset.meta[key])
 
                 asset["file/size"]  = fsize
                 asset["file/mtime"] = fmtime
-
-                #########################################
-                ## PROBE
+                asset["file/ctime"] = int(os.path.getmtime(full_path))
 
                 for probe in probes:
                     if probe.accepts(asset):
                         logging.debug("Probing {} using {}".format(asset, probe))
-                        asset = probe.work(asset)
-
-                ## PROBE
-                #########################################
+                        asset = probe(asset)
 
                 if asset["status"] == RESET:
                     asset["status"] = ONLINE
@@ -120,10 +106,22 @@ class Service(BaseService):
             asset["status"] = ONLINE
             asset.save()
 
-            db = DB()
-            db.query("""UPDATE nx_jobs SET progress=-1, id_service=0, ctime=%s, stime=0, etime=0, id_user=0, message='Restarting after source update'
-                    WHERE id_object=%s AND id_action > 0 and progress IN (-2, -3)""",
-                    [time.time(), id_asset]
-                    )
-
-            db.commit()
+#TODO:
+#            db = DB()
+#            db.query("""
+#                UPDATE nx_jobs SET
+#                    progress=-1,
+#                    id_service=0,
+#                    ctime=%s,
+#                    stime=0,
+#                    etime=0,
+#                    id_user=0,
+#                    message='Restarting after source update'
+#                WHERE
+#                    id_object=%s
+#                AND
+#                    id_action > 0
+#                AND
+#                    progress IN (-2, -3)""", [time.time(), id_asset]
+#                )
+#            db.commit()
--- services/meta/common.py
+++ services/meta/common.py
@@ -6,16 +6,9 @@ class Probe(object):
     def __repr__(self):
         return self.title
 
+    def __call__(self, asset):
+        return asset
+
     def accepts(self, asset):
         return False
 
-    def work(self, asset):
-        return asset
-
-
-def guess_aspect (w, h):
-    if 0 in [w, h]:
-        return 0
-    valid_aspects = [(16, 9), (4, 3), (2.35, 1)]
-    ratio = float(w) / float(h)
-    return "%s/%s" % min(valid_aspects, key=lambda x:abs((float(x[0])/x[1])-ratio))
