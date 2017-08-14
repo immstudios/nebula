e2d7a652ba9a3651d6a2e8f0bf08da7a0aceb96f
Martastain <martas@imm.cz>
2017-08-14 17:22:30 +0200

    updated rex

--- rex.py
+++ rex.py
@@ -1,3 +1,5 @@
+__all__ = ["require"]
+
 import os
 import sys
 import subprocess
@@ -22,9 +24,6 @@ else:
     decode_if_py3 = lambda x: x
     encode_if_py3 = lambda x: x
     string_type = unicode
-
-# do not import anything
-__all__ = []
 
 DEBUG, INFO, WARNING, ERROR, GOOD_NEWS = range(5)
 PLATFORM = "windows" if sys.platform == "win32" else "unix"
@@ -105,7 +104,7 @@ def critical_error(msg, **kwargs):
 
 
 
-class Repository():
+class Repository(object):
     def __init__(self, parent,  url, **kwargs):
         self.parent = parent
         self.url = url
@@ -171,7 +170,7 @@ class Rex(object):
         old_rex = open("rex.py").read()
         if new_rex != old_rex:
             logging.info("Updating REX core")
-            with open("rex_new.py", "w") as f:
+            with open("rex.py", "w") as f:
                 f.write(new_rex)
         else:
             logging.info("REX is up to date")
@@ -213,7 +212,12 @@ class Rex(object):
         return True
 
     def post_install(self, repo):
-        if repo.get("python-path") and not repo.path in sys.path:
+        if (repo.get("python-path") or repo.get("python_path")) and not repo.path in sys.path:
             sys.path.insert(0, repo.path)
 
 rex = Rex()
+
+def require(url, **kwargs):
+    repo = Repository(rex, url, **kwargs)
+    return rex.update(repo) and rex.post_install(repo)
+
