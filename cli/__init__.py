6d94325dee775ce7befc61bd6cbf2f383888b70d
Martastain <martas@imm.cz>
2017-10-14 23:10:16 +0200

    password change cli command

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,5 +1,6 @@
 import cli.run
 import cli.adduser
+import cli.passwd
 import cli.a
 import cli.s
 import cli.j
@@ -9,6 +10,7 @@ __all__ = ["modules"]
 modules = {
         "run" : run.run,
         "adduser" :  adduser.adduser,
+        "passwd" :  passwd.passwd,
         "a" : a.a,
         "s" : s.s,
         "j" : j.j
