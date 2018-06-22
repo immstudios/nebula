427e7e394e9ac8b66fb29c079f3b21b452b16aa2
Martastain <martas@imm.cz>
2018-06-22 22:06:39 +0200

    added delete api method

--- hub/__init__.py
+++ hub/__init__.py
@@ -93,6 +93,7 @@ hub_config = {
         "api_methods" : {
                 "get" : api_get,
                 "set" : api_set,
+                "delete" : api_delete,
                 "settings" : api_settings,
                 "rundown" : api_rundown,
                 "order" : api_order,
