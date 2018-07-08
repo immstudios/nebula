18d8e32e34c682e6bc6669d3030c29b2a3fec9db
Martastain <martas@imm.cz>
2018-07-08 21:05:14 +0200

    mcr acl

--- nx/api/playout.py
+++ nx/api/playout.py
@@ -7,17 +7,19 @@ __all__ = ["api_playout"]
 def api_playout(**kwargs):
     if not kwargs.get("user", None):
         return {'response' : 401, 'message' : 'unauthorized'}
-    user = User(meta=kwargs["user"])
-    #TODO: ACL HERE
 
+    user = User(meta=kwargs["user"])
     action = kwargs.get("action", False)
     id_channel = int(kwargs.get("id_channel", False))
 
-    if not action in ["cue", "take", "abort", "freeze", "retake", "plugin_list", "plugin_exec", "stat", "recover"]:
-        return {'response' : 400, 'message' : 'Unsupported action {}'.format(action)}
     if not id_channel in config["playout_channels"]:
         return {'response' : 400, 'message' : 'Unknown channel {}'.format(id_channel)}
 
+    if not user.has_right("mcr", id_channel):
+        return {'response' : 403, 'message' : 'You are not permitted to operate this channel'}
+
+    if not action in ["cue", "take", "abort", "freeze", "retake", "plugin_list", "plugin_exec", "stat", "recover"]:
+        return {'response' : 400, 'message' : 'Unsupported action {}'.format(action)}
     channel_config = config["playout_channels"][id_channel]
 
     controller_url = "http://{}:{}".format(
