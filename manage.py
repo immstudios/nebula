898e80a7fed6794a15f278e69e26f11e9e9e43ff
Martastain <martas@imm.cz>
2018-08-27 21:49:58 +0200

    manage aliases support

--- manage.py
+++ manage.py
@@ -16,7 +16,12 @@
 #    along with Nebula. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
 import sys
+
+orig_dir = os.getcwd()
+if orig_dir != "/opt/nebula":
+    os.chdir("/opt/nebula")
 
 from nebula import *
 from cli import *
@@ -24,10 +29,22 @@ from cli import *
 logging.user = "Manager"
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        critical_error("This command takes at least one argument")
-    module = sys.argv[1]
-    args = sys.argv[2:]
-    if not module in modules:
-        critical_error("Unknown module '{}'".format(module))
-    modules[module](*args)
+    command = os.path.basename(sys.argv[0])
+
+    if command.startswith("nx"):
+        module = command[2:]
+        args = sys.argv[1:]
+    else:
+        if len(sys.argv) < 2:
+            critical_error("This command takes at least one argument")
+        module = sys.argv[1]
+        args = sys.argv[2:]
+        if not module in modules:
+            critical_error("Unknown module '{}'".format(module))
+
+    try:
+        modules[module](*args)
+    except:
+        log_traceback()
+
+os.chdir(orig_dir)
