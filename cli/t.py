1b336873250aac7b9b4909924559fb92f7bd0a7d
Martastain <martas@imm.cz>
2018-10-05 19:32:26 +0200

    tool execution fixes

--- cli/t.py
+++ cli/t.py
@@ -10,9 +10,14 @@ def t(*args):
     try:
         fp, pathname, description = imp.find_module(plugin_name, [tools_dir])
     except ImportError:
-        critical_error ("unable to locate module: " + plugin_name)
+        critical_error("unable to locate module: " + plugin_name)
 
     try:
         module = imp.load_module(plugin_name, fp, pathname, description)
-    except Exception as e:
-        print (e)
+    except Exception :
+        log_traceback()
+        critical_error("Unable ot open tool " + plugin_name)
+
+    logging.user = plugin_name
+    margs = args[1:] if len(args) > 1 else []
+    module.Plugin(*margs)
