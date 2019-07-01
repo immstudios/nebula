1cb33811c2333667b6d9fc76bc546ffb6e7c77d1
Martastain <martas@imm.cz>
2019-07-01 16:12:24 +0200

    webotools fix

--- nx/plugins/__init__.py
+++ nx/plugins/__init__.py
@@ -4,3 +4,4 @@ from .playout import PlayoutPlugin, PlayoutPluginSlot
 from .worker import WorkerPlugin
 from .validator import ValidatorPlugin
 from .solver import SolverPlugin
+from .webtool import WebToolPlugin
