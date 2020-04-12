8040fea56dcef6c72db61adce5878729dd512e47
Martastain <martas@imm.cz>
2020-04-12 14:40:25 +0200

    added GPU metrics (nvidia)

--- nx/system_monitor.py
+++ nx/system_monitor.py
@@ -1,5 +1,7 @@
+import os
 import time
 import psutil
+import subprocess
 
 from nebulacore import *
 
@@ -10,6 +12,66 @@ __all__ = ["SystemMonitor"]
 
 BOOT_TIME = psutil.boot_time()
 NEBULA_START_TIME = time.time()
+
+SMI_PATH = None
+SMI_PATHS = [
+        "c:\\Program Files\\NVIDIA Corporation\\NVSMI\\nvidia-smi.exe",
+        "/usr/bin/nvidia-smi",
+        "/usr/local/bin/nvidia-smi"
+    ]
+
+for f in SMI_PATHS:
+    if os.path.exists(f):
+        SMI_PATH = f
+
+def get_gpu_stats(smi_path, request_modes=["utilization"]):
+    try:
+        rawdata = subprocess.check_output([smi_path, "-q", "-d", "utilization"])
+    except Exception:
+        log_traceback()
+        return {}
+
+    rawdata = rawdata.decode("utf-8")
+
+    modes = [
+            ["Utilization",  "utilization"],
+            ["GPU Utilization Samples", "gpu-samples"],
+            ["Memory Utilization Samples", "mem-samples"],
+            ["ENC Utilization Samples", "enc-samples"],
+            ["DEC Utilization Samples", "dec-samples"],
+        ]
+    result = []
+    gpu_id = -1
+    current_mode = False
+    gpu_stats = {}
+    for line in rawdata.split("\n"):
+        if line.startswith("GPU"):
+            if gpu_id > -1:
+                result.append(gpu_stats)
+
+            gpu_stats = {"id" : line.split(" ")[1].strip()}
+            gpu_id += 1
+        for m, mslug in modes:
+            if line.startswith((" "*4) + m):
+                current_mode = mslug
+                break
+
+        if current_mode in request_modes and line.startswith(" "*8):
+            key, value = line.strip().split(":")
+            key = key.strip()
+            try:
+                value = float(value.strip().split(" ")[0])
+            except:
+                value = 0
+            if current_mode not in gpu_stats:
+                gpu_stats[current_mode] = {}
+            gpu_stats[current_mode][key.lower()] =  value
+
+    if gpu_id > -1:
+        result.append(gpu_stats)
+
+    return result
+
 
 def update_host_info():
     hostname = config["host"]
@@ -38,6 +100,9 @@ def update_host_info():
             "boot_time" : BOOT_TIME,
             "start_time" : NEBULA_START_TIME,
         }
+
+    if SMI_PATH:
+        status["gpu"] = get_gpu_stats(SMI_PATH)
 
     db = DB()
     db.query(
